4998e94de2102d4538f79886f59da6005659962b
Marten <martentyrk@gmail.com>
2019-11-06 16:01:58 +0200

    Added email send file

---
+++ emailSendLogic.py
@@ -0,0 +1,19 @@
+#acc: PasaHinnavaatlus@gmail.com
+#password: projectpasa123
+#appPassword: lzrrolwzktcylhbc
+
+import smtplib
+from email.message import EmailMessage
+def emailSender(subject,body,addresstoWho):
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = "PasaHinnavaatlus@gmail.com"
+    msg["To"] = addresstoWho
+    msg.set_content(body)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com",465) as smtp:
+        smtp.login("PasaHinnavaatlus@gmail.com","lzrrolwzktcylhbc")
+
+        smtp.send_message(msg)
+
+
