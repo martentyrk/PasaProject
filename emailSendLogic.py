221dca15725f3f6659ae49f68d7b560892e9e68a
Marten <martentyrk@gmail.com>
2019-11-06 16:07:46 +0200

    small changes in email file

--- emailSendLogic.py
+++ emailSendLogic.py
@@ -2,8 +2,13 @@
 #password: projectpasa123
 #appPassword: lzrrolwzktcylhbc
 
+#peaks võibolla ehitama üles nii et saadame meili
+#HTMLi look'iga. Näeb parem välja ja rohkem customizable
+#https://www.youtube.com/watch?v=JRCJ6RtE3xU selle abil tegin, HTML osa on 29:55
+
 import smtplib
 from email.message import EmailMessage
+
 def emailSender(subject,body,addresstoWho):
     msg = EmailMessage()
     msg["Subject"] = subject
@@ -13,7 +18,6 @@ def emailSender(subject,body,addresstoWho):
 
     with smtplib.SMTP_SSL("smtp.gmail.com",465) as smtp:
         smtp.login("PasaHinnavaatlus@gmail.com","lzrrolwzktcylhbc")
-
         smtp.send_message(msg)
 
 
