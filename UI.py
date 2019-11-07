d7c18abc9479a35dc1ff9854dbc4034a40b65467
Marten <martentyrk@gmail.com>
2019-11-07 12:25:24 +0200

    Made a basic logic behind save and a checker if email needs to be sent

--- UI.py
+++ UI.py
@@ -1,19 +1,19 @@
 import tkinter as tk
 
+def UserInterface():
+    main = tk.Tk()
+    main["bg"] = "#FFFFFF"
 
-main = tk.Tk()
-main["bg"] = "#FFFFFF"
 
+    rootWidth = main.winfo_width()
+    rootHeight = main.winfo_height()
+    c = tk.Canvas(main,cursor="leftbutton", width=rootWidth + 500, height=rootHeight+300, bg="#9CBEC1", confine=True)
+    entry = tk.Entry(main)
 
-rootWidth = main.winfo_width()
-rootHeight = main.winfo_height()
-c = tk.Canvas(main,cursor="leftbutton", width=rootWidth + 500, height=rootHeight+300, bg="#9CBEC1", confine=True)
-entry = tk.Entry(main)
+    entry.insert(0, "neti.ee")
 
-entry.insert(0, "neti.ee")
-
-entry.pack()
-c.pack(expand=True)
-main.minsize(600, 400)
-main.maxsize(800, 600)
-main.mainloop()
+    entry.pack()
+    c.pack(expand=True)
+    main.minsize(600, 400)
+    main.maxsize(800, 600)
+    main.mainloop()
--- webscraping.py
+++ webscraping.py
@@ -3,12 +3,14 @@ import bs4
 
 def webscrape(webURL):
     URL = webURL
-    page = requests.get(URL, headers = {"User-Agent":"Undefined"})
+    try:
+        page = requests.get(URL, headers = {"User-Agent":"Undefined"})
+    except: return("Olete sisestanud vale URLi, proovige uuesti")
     soup = bs4.BeautifulSoup(page.content,"html.parser")
     try:
         priceInfo = soup.find(id="priceblock_saleprice").get_text()
     except:
         try: priceInfo = soup.find(id="priceblock_ourprice").get_text()
-        except:
-            print("Olete sisestanud vale URLi, proovige uuesti")
+        except:NameError
     return (priceInfo)
+
