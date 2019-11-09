127d1cd31ab57466aaa19f58ec8c0f26087aea7b
Marten <martentyrk@gmail.com>
2019-11-09 19:57:09 +0200

    Added checker if webpage exists and made a function for scaperlogic

--- webscraping.py
+++ webscraping.py
@@ -16,26 +16,29 @@ def germanTranslator(word):
 def webscrape(URL):
     productInfo = {}
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15"}
+    existanceChecker = requests.get(URL)
+    if existanceChecker.status_code == 200:
+        page = requests.get(URL,headers=headers)
 
-    page = requests.get(URL,headers=headers)
+        time.sleep(2)
+        soup = bs4.BeautifulSoup(page.content, "html.parser")
+        time.sleep(5)
+        #added time.sleep so that we would not be recognized as robots
+        productTitle = soup.find(id="productTitle").get_text().strip()
+        productAvailability = soup.find(id="availability").get_text().strip()
+        #getting product title and availability status
+        #trying different id tags because different products have different ids depending on sale
+        try:
+            productPrice = soup.find(id="priceblock_ourprice").get_text()
+        except:
+            try: productPrice = soup.find(id="priceblock_dealprice").get_text()
 
-    time.sleep(2)
-    soup = bs4.BeautifulSoup(page.content, "html.parser")
-    time.sleep(5)
-    #added time.sleep so that we would not be recognized as robots
-    productTitle = soup.find(id="productTitle").get_text().strip()
-    productAvailability = soup.find(id="availability").get_text().strip()
-    #getting product title and availability status
-    #trying different id tags because different products have different ids depending on sale
-    try:
-        productPrice = soup.find(id="priceblock_ourprice").get_text()
-    except:
-        try: productPrice = soup.find(id="priceblock_dealprice").get_text()
+            except: return("Sisestatud URL millel puudub hind.")
+        #adding all the needed info to productInfo
+        productAvailability = germanTranslator(productAvailability)
+        productInfo["availability"] = productAvailability
+        productInfo["price"] = productPrice
+        productInfo["title"] = productTitle
+        return productInfo
 
-        except: return("Sisestatud vale hind")
-    #adding all the needed info to productInfo
-    productAvailability = germanTranslator(productAvailability)
-    productInfo["availability"] = productAvailability
-    productInfo["price"] = productPrice
-    productInfo["title"] = productTitle
-    return productInfo
+    else: return "Sellist URLi ei eksisteeri"
