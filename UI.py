313bb4f44c4be8626d456825886e42c5cf9ed1af
siimlangel <siimlangel11@gmail.com>
2019-11-11 12:11:17 +0200

    Modified UI

--- UI.py
+++ UI.py
@@ -1,21 +1,36 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+import webscraping as web
 
+#info = web.webscrape("https://www.amazon.com/PlayStation-4-Slim-1TB-Console/dp/B071CV8CG2/ref=sr_1_1?keywords=playstation+4&qid=1573324821&sr=8-1")
+
+
+
+productinfo_string = "Tere"
 
 def userInterface():
     main = tk.Tk()
     main["bg"] = "#FFFFFF"
     main.geometry("600x400")
+    main.title("Price tracker")
 
     canvas = tk.Canvas(main, width=main.winfo_width() * 600, height=main.winfo_height() * 400, bg="#25FE34")
     background = ImageTk.PhotoImage(Image.open("UIbackground.png").resize((600, 450)))
     background_label = tk.Label(canvas, image=background)
     background_label.place(x=0, y=0, relwidth=1, relheight=1.0)
 
+    productinfo_label = tk.Label(canvas, bg="#FFFFFF", width=43, height=10, text=productinfo_string)
+
     URLentry = tk.Entry(canvas, width=50, bd=0)
     URLentry.insert(0, "neti.ee")
 
-    URLentry.place(x=150, y=100)
+    get_info_button = tk.Button(canvas, text="Get info", width=10)
+    track_button = tk.Button(canvas, text="Track", width=10)
+
+    productinfo_label.place(x=150, y=200)
+    track_button.place(x=270, y=150)
+    get_info_button.place(x=270, y=100)
+    URLentry.place(x=150, y=50)
     canvas.place(x=0, y=0)
     main.minsize(600, 400)
     main.maxsize(600, 400)
--- webscraping.py
+++ webscraping.py
@@ -2,7 +2,8 @@ import requests
 import bs4
 import time
 
-#translator for amazon.de availability
+
+# translator for amazon.de availability
 def germanTranslator(word):
     if word == "Auf lager.":
         word = "In stock."
@@ -15,30 +16,32 @@ def germanTranslator(word):
 
 def webscrape(URL):
     productInfo = {}
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15"}
-    existanceChecker = requests.get(URL)
-    if existanceChecker.status_code == 200:
-        page = requests.get(URL,headers=headers)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
+    existenceChecker = requests.get(URL)
 
-        time.sleep(2)
-        soup = bs4.BeautifulSoup(page.content, "html.parser")
-        time.sleep(5)
-        #added time.sleep so that we would not be recognized as robots
-        productTitle = soup.find(id="productTitle").get_text().strip()
-        productAvailability = soup.find(id="availability").get_text().strip()
-        #getting product title and availability status
-        #trying different id tags because different products have different ids depending on sale
+    page = requests.get(URL, headers=headers)
+
+    time.sleep(2)
+    soup = bs4.BeautifulSoup(page.content, "html.parser")
+
+    time.sleep(5)
+    # added time.sleep so that we would not be recognized as robots
+    productTitle = soup.find(id="productTitle").get_text().strip()
+    productAvailability = soup.find(id="availability").get_text().strip()
+    # getting product title and availability status
+    # trying different id tags because different products have different ids depending on sale
+    try:
+        productPrice = soup.find(id="priceblock_ourprice").get_text()
+    except:
         try:
-            productPrice = soup.find(id="priceblock_ourprice").get_text()
+            productPrice = soup.find(id="priceblock_dealprice").get_text()
+
         except:
-            try: productPrice = soup.find(id="priceblock_dealprice").get_text()
-
-            except: return("Sisestatud URL millel puudub hind.")
-        #adding all the needed info to productInfo
-        productAvailability = germanTranslator(productAvailability)
-        productInfo["availability"] = productAvailability
-        productInfo["price"] = productPrice
-        productInfo["title"] = productTitle
-        return productInfo
-
-    else: return "Sellist URLi ei eksisteeri"
+            return ("Sisestatud URL millel puudub hind.")
+    # adding all the needed info to productInfo
+    productAvailability = germanTranslator(productAvailability)
+    productInfo["availability"] = productAvailability
+    productInfo["price"] = productPrice
+    productInfo["title"] = productTitle
+    return productInfo
