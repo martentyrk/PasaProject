7ff6936fada954480665bdda6c04538768539eb5
Marten <martentyrk@gmail.com>
2019-12-11 15:13:13 +0200

    added ebay extension

--- webscraping.py
+++ webscraping.py
@@ -22,7 +22,8 @@ def webscrape(URL):
     existenceChecker = requests.get(URL)
 
     page = requests.get(URL, headers=headers)
-    print(page)
+
+
     soup = bs4.BeautifulSoup(page.content, "html5lib")
 
 
@@ -30,23 +31,77 @@ def webscrape(URL):
 
     # added time.sleep so that we would not be recognized as robots
 
-    productTitle = soup.find(id="productTitle").get_text().strip()
-
-
-    productAvailability = soup.find(id="availability").get_text().strip()
-    # getting product title and availability status
-    # trying different id tags because different products have different ids depending on sale
-    try:
-        productPrice = soup.find(id="priceblock_ourprice").get_text()
-    except:
+#Amazoni kohta webscrape
+    if "amazon" in URL:
         try:
-            productPrice = soup.find(id="priceblock_dealprice").get_text()
-
+            productTitle = soup.find(id="productTitle").get_text().strip()
         except:
-            return ("Sisestatud URL millel puudub hind.")
-    # adding all the needed info to productInfo
-    productAvailability = germanTranslator(productAvailability)
-    productInfo["title"] = productTitle
-    productInfo["availability"] = productAvailability
-    productInfo["price"] = productPrice
-    return productInfo
+            try:
+                productTitle = soup.find(id="productTitle").get_text().strip()
+            except:
+                productTitle = " "
+
+        try:
+            productAvailability = soup.find(id="productAvailability").get_text.strip()
+        except:
+            try:
+                productAvailability = soup.find("p", class_="a-spacing-micro a-color-secondary a-text-bold").text.strip()
+            except:
+                productAvailability = ""
+
+        # getting product productTitle and productAvailability status
+        # trying different id tags because different products have different ids depending on sale
+        try:
+            productPrice = soup.find(id="priceblock_ourprice").get_text()
+        except:
+            try:
+                productPrice = soup.find(id="priceblock_dealprice").get_text()
+
+            except:
+                return ("Sisestatud URL millel puudub hind.")
+        # adding all the needed info to productInfo
+        productAvailability = germanTranslator(productAvailability)
+        productInfo["availability"] = productAvailability
+        productInfo["price"] = productPrice
+        productInfo["title"] = productTitle
+
+        return(productInfo)
+
+#Ebay scanner
+    if "ebay" in URL:
+        #getting the productTitle and removing a small part from it.
+        try:
+            h1 = soup.find("h1", id="itemTitle").find("span").get_text()
+            productTitle = soup.find("h1", id="itemTitle").get_text()
+            if h1 in productTitle:
+                productTitle = productTitle.replace(h1,"")
+                productInfo["title"] = productTitle
+            else:
+                productInfo["title"] = productTitle
+        except:
+            productInfo["title"] = " "
+
+        #getting item productPrice
+        try:
+            productPrice = soup.find("span", id="prcIsum").text
+            productInfo["price"] = productPrice
+        except:
+            try:
+                productPrice = soup.find("span", id="mm-saleDscPrc").text
+                productInfo["price"] = productPrice
+
+            except:
+                productInfo["price"] = " "
+
+        #getting the productAvailability
+        try:
+            productAvailability = soup.find("span", id="qtySubTxt").get_text().strip()
+            productInfo["availability"] = productAvailability
+        except:
+            productInfo["availability"] = " "
+
+        return(productInfo)
+
+    else:
+        return("Please use an amazon or ebay website")
+
